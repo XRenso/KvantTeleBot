ebb73f03061142f1dd9f8a287352f895ec8c54bd
XRenso <70619857+XRenso@users.noreply.github.com>
2021-02-01 20:10:35 +1100

    syntax fix

--- kvant_bot/bot.py
+++ kvant_bot/bot.py
@@ -48,7 +48,7 @@ admin = False
 #MR2 - строго обрабатывать действия пользователя связанных с админ доступом
   
 
-@client.message_handler(commands = ['start'])
+@client.message_handler(commands = ['get_id'])
 def user_id(message):
 	client.send_message(message.chat.id, message.chat.id)
 @client.message_handler(commands = ['start'])
