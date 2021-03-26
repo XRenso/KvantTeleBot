68c8cc3f00d3004ed0f4c0ada26df153afb1c480
XRenso <70619857+XRenso@users.noreply.github.com>
2021-03-26 05:04:46 +0300

    bug fix
    21

--- kvant_bot/config.py
+++ kvant_bot/config.py
@@ -1,11 +1,6 @@
-import os
+
 
 token = "1411084539:AAEQa6SoEGPtrvwC_Kp_1g-PK2MLLxr1_y8"
-feed = 'http://kvantorium.iroso.ru/feed'
 
-joinedFile = open('joined.txt', 'r')
-joinedUsers = set(joinedFile)
-lines = joinedFile.readlines()
-for line in lines:
-	joinedUsers.add(line.strip())
-joinedFile.close()
+
+
